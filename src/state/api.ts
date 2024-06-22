d6a0d59a8e667d4e15d8e658ef29a27b4d17350d
Rajarshi Saha <saharajarshi14@gmail.com>
2024-06-22 19:32:54 +0530

    Dummy commit due to some issues

--- src/state/api.ts
+++ src/state/api.ts
@@ -9,6 +9,7 @@ export const api = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_BASE_URL }),
   reducerPath: "main",
   tagTypes: ["Kpis", "Products", "Transactions"],
+  
   endpoints: (build) => ({
     getKpis: build.query<Array<GetKpisResponse>, void>({
       query: () => "kpi/kpis/",
